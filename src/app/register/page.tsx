ba8c6745dea3b13495f36a7034c04ed2ba940e84
Ahemd Faysal <fr.abir.ahemd.faysal@gmail.com>
2025-09-19 13:47:08 +0600

    register test

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+"use client";
+import React, { FormEvent } from "react";
+import { signIn, useSession } from "next-auth/react";
 
 const Register = () => {
+  const session = useSession();
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const promise = await signIn("credentials", {
+      redirect: false,
+    });
+
+    console.log(promise);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-950 text-white">
       <div className="w-full max-w-md bg-gray-900 rounded-2xl shadow-lg p-8">
-        <h2 className="text-2xl font-bold mb-6 text-center">Create an account</h2>
-        <form className="space-y-5">
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          Create an account
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-5">
           <div>
             <label className="block mb-2 text-sm">User Name</label>
             <input
               type="text"
               placeholder="Enter your name"
+              name="username"
               className="w-full px-4 py-2 rounded-lg bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
@@ -20,6 +37,7 @@ const Register = () => {
             <input
               type="email"
               placeholder="Enter your email"
+              name="email"
               className="w-full px-4 py-2 rounded-lg bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
@@ -29,6 +47,7 @@ const Register = () => {
             <input
               type="password"
               placeholder="Enter your password"
+              name="password"
               className="w-full px-4 py-2 rounded-lg bg-gray-800 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
@@ -46,6 +65,11 @@ const Register = () => {
             Login
           </a>
         </p>
+        {session.status === "authenticated" && (
+          <div className="mt-4 text-green-400">
+            Logged in as: {JSON.stringify(session.data.user)}
+          </div>
+        )}
       </div>
     </div>
   );
